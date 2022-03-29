b727627e3028c3fdf027db6c72607ca93e8185be
Tanapatjeensamran <imanuglypanda@gmail.com>
2022-03-29 13:58:32 +0700

    change

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() { // ES6 Shorthand
+        return {
+            product: 'Boots',
+            description: 'A heavy shoe that covers part of the leg.'
+        }
+    }
+})
